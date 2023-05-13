9e1231041dcb89789f045ac643410807bc0c48aa
Sophie LAURENT <sophielaurent92@gmail.com>
2023-05-13 09:03:02 +0200

    Initial commit

---
+++ Poseidon/src/test/java/com/nnk/springboot/Poseidon/PoseidonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnk.springboot.Poseidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
